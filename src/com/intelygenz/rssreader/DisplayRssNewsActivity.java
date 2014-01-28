9e5cd2f43c7e09c7cd0723f33a04fda492893145
Jie Lee <zan.jie.lee@gmail.com>
2014-01-28 14:37:23 +0100

    remove imports

--- src/com/intelygenz/rssreader/DisplayRssNewsActivity.java
+++ src/com/intelygenz/rssreader/DisplayRssNewsActivity.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ListView;
